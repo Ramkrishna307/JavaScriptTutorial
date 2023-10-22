8f82cc78916de9bde8c31373919eedeb9e0378e8
Ramkrishna Roy <ramkrishnaroy324@gmail.com>
2023-10-22 15:19:55 +0530

    ...
    Signed-off-by: Ramkrishna Roy <ramkrishnaroy324@gmail.com>

---
+++ Dom/main.js
@@ -0,0 +1,5 @@
+//if you want to acess the dom object then           console.log(window)
+
+// that will give you the window object 
+
+// To access the window object run    console.log(window.doument)
