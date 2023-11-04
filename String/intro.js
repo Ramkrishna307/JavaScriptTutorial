caf9fb738dbede5cfb4aaf6f5a146c283444c4f9
Ramkrishna Roy <ramkrishnaroy324@gmail.com>
2023-11-05 00:56:18 +0530

    ..
    Signed-off-by: Ramkrishna Roy <ramkrishnaroy324@gmail.com>

---
+++ String/intro.js
@@ -0,0 +1,53 @@
+// console.log("Ram\nSam");
+// console.log("Ram\tSam");
+// console.log("Ram\Sam");
+
+// console.log("Ram says\"EDvery day is a ...........\"");
+
+// let a="delhi";
+
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+
+// for (let i=0;i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+
+let str="kolkata";
+let c=str.startsWith('kol')
+console.log(str.endsWith("kata"));
+console.log(c);
+
+// by default case sensitive 
+
+let str2="Good Morning";
+
+let sub=str2.slice(0,4);
+let sub2=str2.substring(0,4)
+console.log(sub2);
+ console.log(sub)
+
+
+ //1st case
+ const arr=["Ram","Roy"]
+ console.log(arr);
+
+//  2nd case
+const a=[];
+a[0]="Ram"
+a[1]="Roy"
+console.log(a);
+
+//3rd  case
+let a2=new Array("ram","roy");
+console.log(a2);
+
+
+
+const err=[];
+for(let i=0;i<10;i++){
+    arr[i]=i
+}
+console.log(arr);
